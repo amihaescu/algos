288792b4046285af2921d4308661833ced0e1cc5
amihaescu <andrei.mihaescu91@gmail.com>
2019-11-02 17:29:37 +0200

    added connected nodes functionality

--- src/main/java/ro/amihaescu/algos/BFS.java
+++ src/main/java/ro/amihaescu/algos/BFS.java
@@ -1,21 +1,20 @@
 package ro.amihaescu.algos;
 
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BFS {
 
     private Graph graph;
-    private Queue<Integer> queue = new ArrayDeque<>();
-    int[] prev;
+    private Map<Integer, Integer> prev;
 
     public BFS(Graph graph) {
         this.graph = graph;
-        prev = new int[graph.getSize()];
     }
 
     private void traverse(int node) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        prev = new LinkedHashMap<>();
         queue.add(node);
         boolean[] visited = new boolean[graph.getSize()];
         visited[node] = true;
@@ -27,29 +26,47 @@ public class BFS {
             for (Integer neighbour : neighbours) {
                 if (!visited[neighbour]) {
                     queue.add(neighbour);
-                    System.out.print(neighbour + " ");
                     visited[neighbour] = true;
-                    prev[neighbour] = node;
+                    prev.put(neighbour, node);
                 }
             }
         }
     }
 
+    private String reconstructPath(int startingNode, int endNode) {
+        traverse(startingNode);
+        List<Integer> path = new ArrayList<>();
+
+        for (Integer node = endNode; node != null; node = prev.get(node)) {
+            path.add(node);
+        }
+
+        Collections.reverse(path);
+        if (path.get(0).equals(startingNode)) {
+            return "Nodes connected: " +
+                    path.stream().map(integer -> integer + "").collect(Collectors.joining(" "));
+
+        } else {
+            return "Nodes not connected";
+        }
+    }
+
     public static void main(String[] args) {
         Graph graph = new Graph(10);
-        graph.addEdge(0,3);
-        graph.addEdge(0,1);
-        graph.addEdge(0,2);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
 
-        graph.addEdge(3,5);
-        graph.addEdge(1,6);
-        graph.addEdge(2,4);
+        graph.addEdge(3, 5);
+        graph.addEdge(1, 6);
+        graph.addEdge(2, 4);
 
-        graph.addEdge(6,9);
-        graph.addEdge(6,8);
-        graph.addEdge(4,7);
+        graph.addEdge(6, 9);
+        graph.addEdge(6, 8);
+        graph.addEdge(4, 7);
         BFS bfs = new BFS(graph);
         bfs.traverse(0);
+        System.out.println(bfs.reconstructPath(3, 7));
     }
 
 }
